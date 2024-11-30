d48c57f0ba2b48fe181df6cd5100a54a1565daa0
philip <ajibolaphilip10@gmail.com>
2024-11-30 14:37:18 +0100

    commit

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -3,9 +3,12 @@ import Reviews from "../components/reviews.jsx";
 import Advert from "../components/advert.jsx";
 import RecentlyAdded from "../components/recentlyAdded.jsx";
 import Hero from "../components/hero.jsx";
+import '../styles/overideStyles.css'
+import Navbar from "../components/navbar.jsx";
 export default function HomePage(){
     return (
-        <div>
+        <div >
+            <Navbar/>
             <Hero/>
             <RecentlyAdded/>
             <Advert/>
