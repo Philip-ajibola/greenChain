1c74242a6ecda394c915398b0b27d7ddb712002c
Biokes <106444765+Biokes@users.noreply.github.com>
2024-11-30 13:03:42 +0100

    update dependencies

--- src/main.jsx
+++ src/main.jsx
@@ -3,11 +3,14 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 import {BrowserRouter} from "react-router-dom";
+import {EnokiFlowProvider} from "@mysten/enoki/react";
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
       <BrowserRouter>
-          <App />
+          <EnokiFlowProvider  apiKey="enoki_public_8db3e6200e6ff6bc10945b09b149f09c">
+              <App />
+          </EnokiFlowProvider>
       </BrowserRouter>
   </StrictMode>,
 )
